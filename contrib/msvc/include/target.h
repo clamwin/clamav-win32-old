65b7a2c5a1b406e935c4d983cf5bcc732fa820ae
sherpya <sherpya@netfarm.it>
2009-07-26 20:49:48 +0200

    - fixes for mingw, missing target.h and Makefile

---
+++ contrib/msvc/include/target.h
@@ -0,0 +1,13 @@
+/* Clamav specific */
+
+#ifndef TARGET_OS_TYPE
+#define TARGET_OS_TYPE "win32"
+#endif
+
+#ifndef TARGET_ARCH_TYPE
+#define TARGET_ARCH_TYPE "x86"
+#endif
+
+#ifndef TARGET_CPU_TYPE
+#define TARGET_CPU_TYPE "i686"
+#endif
