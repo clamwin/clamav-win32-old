8e02f2726db4e33c12b012541e0b39846b14aee9
sherpya <sherpya@netfarm.it>
2010-04-20 22:45:54 +0200

    synced with clamav-config.h.in

--- contrib/msvc/clamav-config.h
+++ contrib/msvc/clamav-config.h
@@ -21,6 +21,9 @@
 /* enable clamuko */
 #undef CLAMUKO
 
+/* disable loading of unsigned bytecode */
+#undef CL_BCUNSIGNED
+
 /* enable debugging */
 #ifdef _DEBUG
 #define CL_DEBUG 1
@@ -394,6 +397,9 @@
 
 /* Define to 1 if you have the <termios.h> header file. */
 #define HAVE_TERMIOS_H 1
+
+/* Define this if uname(2) is POSIX */
+#undef HAVE_UNAME_SYSCALL
 
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
