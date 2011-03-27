485661f1a3b2d123d486e7c89ae1a71e68520f66
sherpya <sherpya@netfarm.it>
2011-03-27 06:12:32 +0200

    I need to copy the name of the file being scanned because the engine can call free() on e.g. temp names

--- contrib/msvc/src/dllmain/tls.c
+++ contrib/msvc/src/dllmain/tls.c
@@ -19,55 +19,29 @@
  */
 
 #include <osdeps.h>
-#include <pthread.h>
 #include <others.h>
 
-// __declspec(thread) are not happy with python, maybe different crt?
-//#ifdef _MSC_VER
-//#define HAVE_DECLSPEC_THREAD
-//#endif
-
-#ifdef HAVE_DECLSPEC_THREAD
-static __declspec(thread) const char *__currentfile = NULL;
-static __declspec(thread) PVOID state = NULL;
-
-void tls_alloc(void) {}
-void tls_free(void) {}
-
-const char *cw_get_currentfile(void)
-{
-    return __currentfile;
-}
-
-void cw_set_currentfile(const char *filename)
-{
-    __currentfile = filename;
-}
-
-BOOL cw_disablefsredir(void)
-{
-    cli_dbgmsg("[tls] DISABLED REDIR for %d\n", GetCurrentThreadId());
-    return cw_helpers.k32.Wow64DisableWow64FsRedirection(&state);
-}
-
-BOOL cw_revertfsredir(void)
-{
-    cli_dbgmsg("[tls] REVERTED REDIR for %d\n", GetCurrentThreadId());
-    return cw_helpers.k32.Wow64RevertWow64FsRedirection(&state);
-}
-
-#else
 static DWORD __currentfile_idx = TLS_OUT_OF_INDEXES;
 static DWORD __fsredir_idx = TLS_OUT_OF_INDEXES;
 
 void tls_alloc(void)
 {
+    char *fptr;
+
     __currentfile_idx = TlsAlloc();
     if (__currentfile_idx == TLS_OUT_OF_INDEXES)
     {
         cli_errmsg("[tls] Unable to allocate Tls slot for currentfile storage: %d\n", GetLastError());
         exit(1);
     }
+
+    if (!(fptr = VirtualAlloc(NULL, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE)))
+    {
+        cli_errmsg("[tls] Unable to allocate memory for currentfile storage: %d\n", GetLastError());
+        exit(1);
+    }
+
+    TlsSetValue(__currentfile_idx, (LPVOID) fptr);
 
     __fsredir_idx = TlsAlloc();
     if (__fsredir_idx == TLS_OUT_OF_INDEXES)
@@ -79,6 +53,7 @@ void tls_alloc(void)
 
 void tls_free(void)
 {
+    VirtualFree(TlsGetValue(__currentfile_idx), MAX_PATH, MEM_RELEASE);
     TlsFree(__currentfile_idx);
     TlsFree(__fsredir_idx);
 }
@@ -90,7 +65,14 @@ const char *cw_get_currentfile(void)
 
 void cw_set_currentfile(const char *filename)
 {
-    TlsSetValue(__currentfile_idx, (LPVOID) filename);
+    char *fptr = TlsGetValue(__currentfile_idx);
+    if (filename)
+    {
+        strncpy(fptr, filename, MAX_PATH - 1);
+        fptr[MAX_PATH] = 0;
+    }
+    else
+        fptr[0] = 0;
 }
 
 BOOL cw_disablefsredir(void)
@@ -135,4 +117,3 @@ BOOL cw_revertfsredir(void)
     TlsSetValue(__fsredir_idx, NULL);
     return result;
 }
-#endif
