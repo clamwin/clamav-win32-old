06fbbf8f6ada93cb33fd165d18a3ad246a1f23d4
sherpya <sherpya@netfarm.it>
2015-07-05 00:28:13 +0200

    json-c: use vsnprintf on MinGW instead of _vscprintf
    _vscprintf is missing on nt4 mscvrt

--- contrib/msvc/json-c/printbuf.c
+++ contrib/msvc/json-c/printbuf.c
@@ -118,22 +118,22 @@ int printbuf_memset(struct printbuf *pb, int offset, int charvalue, int len)
 /* CAW: compliant version of vasprintf */
 static int vasprintf(char **buf, const char *fmt, va_list ap)
 {
-#ifndef WIN32
+#ifndef _MSC_VER
 	static char _T_emptybuffer = '\0';
-#endif /* !defined(WIN32) */
+#endif /* !defined(_MSC_VER) */
 	int chars;
 	char *b;
 
 	if(!buf) { return -1; }
 
-#ifdef WIN32
+#ifdef _MSC_VER
 	chars = _vscprintf(fmt, ap)+1;
-#else /* !defined(WIN32) */
+#else /* !defined(_MSC_VER) */
 	/* CAW: RAWR! We have to hope to god here that vsnprintf doesn't overwrite
 	   our buffer like on some 64bit sun systems.... but hey, its time to move on */
 	chars = vsnprintf(&_T_emptybuffer, 0, fmt, ap)+1;
 	if(chars < 0) { chars *= -1; } /* CAW: old glibc versions have this problem */
-#endif /* defined(WIN32) */
+#endif /* defined(_MSC_VER) */
 
 	b = (char*)malloc(sizeof(char)*chars);
 	if(!b) { return -1; }
