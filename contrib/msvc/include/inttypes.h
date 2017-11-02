2cedc928d1db8b62e56b5dadf6c712e91d7d556f
sherpya <sherpya@netfarm.it>
2017-11-02 01:39:50 +0100

    inttypes: add missing types needed by cstdint on msvc 2017

--- contrib/msvc/include/inttypes.h
+++ contrib/msvc/include/inttypes.h
@@ -46,6 +46,26 @@ typedef uint8_t  u_int8_t;
 typedef long long          intmax_t;
 typedef unsigned long long uintmax_t;
 
+#ifdef __cplusplus
+typedef signed char        int_least8_t;
+typedef short              int_least16_t;
+typedef int                int_least32_t;
+typedef long long          int_least64_t;
+typedef unsigned char      uint_least8_t;
+typedef unsigned short     uint_least16_t;
+typedef unsigned int       uint_least32_t;
+typedef unsigned long long uint_least64_t;
+
+typedef signed char        int_fast8_t;
+typedef int                int_fast16_t;
+typedef int                int_fast32_t;
+typedef long long          int_fast64_t;
+typedef unsigned char      uint_fast8_t;
+typedef unsigned int       uint_fast16_t;
+typedef unsigned int       uint_fast32_t;
+typedef unsigned long long uint_fast64_t;
+#endif
+
 #ifndef __cplusplus
 typedef uint32_t mode_t;
 #endif
